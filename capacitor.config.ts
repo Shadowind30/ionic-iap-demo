b2b11ff31cc0dd0773ee348e0c85f9e648ab6aad
Shadowind30 <shadowind30@gmail.com>
2022-04-13 12:42:13 -0400

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.sw30.patience',
   appName: 'ionic-iap-demo',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InAppPurchase2 } from '@awesome-cordova-plugins/in-app-purchase-2/ngx';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public patience = 0;
+  private readonly PRODUCT_ID = 'com.sw30.patience';
 
-  constructor() {}
+  constructor(public platform: Platform, private store: InAppPurchase2, private alertCtrl: AlertController) {}
 
+  ngOnInit() {
+    this.refreshStore();
+  }
+
+  public buyPatience() {
+    this.store.order(this.PRODUCT_ID);
+    this.store
+      .when(this.PRODUCT_ID)
+      .approved(async (p) => {
+        p.verify();
+        console.log('approved');
+        const alert = await this.alertCtrl.create({
+          header: 'Compra completada',
+          message: 'Quieres comprar mas? Nunca se tiene suficiente.',
+          buttons: ['Listo']
+        });
+        await alert.present();
+        this.patience += 1;
+      })
+      .verified((p) => {
+        p.finish();
+        console.log('purchase verified');
+      });
+  }
+
+  private refreshStore() {
+    this.platform.ready().then(() => {
+      this.store.verbosity = this.store.DEBUG;
+      this.store.register({
+        id: this.PRODUCT_ID,
+        type: this.store.CONSUMABLE,
+      });
+      this.store.refresh();
+      console.log('store refreshed');
+    });
+  }
 }
